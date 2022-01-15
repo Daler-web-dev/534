2bbd1ffd4e5bc4082c03c2994b91fe9b3dedff4b
Tima7371 <amirjonravshanov@gmail.com>
2022-01-15 20:17:10 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+//  Методы работ с намбер 
+
+
+// console.log(Math.floor(int)); // округление вниз
+// console.log(Math.ceil(int)); // округление вверх
+// console.log(Math.round(int)); // логическое округление 
+// console.log(Math.min(int, 5, 1)); // показывает минимальное число среди остальных чисел 
+// console.log(Math.max(int, 5, 1)); // показывает максимальное число среди остальных чисел 
+// // Math.random() возвращает рандомное число с запятой
+// int.toFixed() // может как округлять так и вырезать числа после запятых 
+// console.log(parseFloat(int)); // переводит из стринга в намбер но оставляет запятые
+// console.log(parseInt(int)); //  переводит из стринга в намбер но не оставляет запятые
+// console.log(+int); // просто перевод из стринга в намбер 
+
+
+
+// let int = 10
+
+// int = int + 1
+// int +=  1
+
+// int = int - 1
+// int -=  1
+
+// int++ // increment // postfix
+// int-- // decrement // postfix
+
+// ++int // increment // prefix
+// --int // decrement // prefix
+
+// console.log(int);
